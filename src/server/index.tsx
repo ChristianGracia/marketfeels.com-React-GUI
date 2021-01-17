0924997e675a1d77184753732ee35d70371e1fa5
Christian Gracia <christiangracia.student@careerdevs.com>
2021-01-17 03:53:14 -0500

    added favicon.ico

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -60,6 +60,10 @@ function main() {
                   <meta charset="utf-8" />
                   <title>MarketFeels.com - See whats being talked about</title>
                   <base href="/" />
+                  <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+                  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favico/favicon-32x32.png">
+                  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favico/favicon-16x16.png">
+                  <link rel="manifest" href="../../assets/favico/site.webmanifest">
 
                   <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <style>
